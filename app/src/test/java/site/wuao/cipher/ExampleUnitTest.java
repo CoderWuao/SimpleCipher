c3f56d9ec8643df8f54579e8c7287874749c533d
吴奡 <215731932@qq.com>
2018-06-19 12:58:24 +0800

    删除readme注释

--- app/src/test/java/site/wuao/cipher/ExampleUnitTest.java
+++ app/src/test/java/site/wuao/cipher/ExampleUnitTest.java
@@ -338,19 +338,12 @@ public class ExampleUnitTest {
 
     @Test
     public void messageDigest() {
-        // md2
         Assert.assertEquals(MD2.digestHex("wuao"),"ab737d2f07588dd5242aaa4b135ed780");
-        // md5
         Assert.assertEquals(MD5.digestHex("wuao"), "08d40236d48c5377f95193190ace250f");
-        // sha1
         Assert.assertEquals(SHA1.digestHex("wuao"), "9ad403bfd80c6af2cff4d1a7b1635cf5bec9a4fe");
-        // sha224
         Assert.assertEquals(SHA224.digestHex("wuao"), "abce923277c0af0a950f2a7325f59294c0139695299f6004cbcc5a5a");
-        // sha256
         Assert.assertEquals(SHA256.digestHex("wuao"), "15d3c1c0b6707fc8ae818ab8f7ffbb0d8f86425c6ab1352da3af6d15219988a6");
-        // sha384
         Assert.assertEquals(SHA384.digestHex("wuao"), "84fd25dca4cadc5b1538b6dea1104bc9e6d8906e957060a1cdb04da5b4b7054c5704e1b176babe5d313966fc781c876b");
-        // sha512
         Assert.assertEquals(SHA512.digestHex("wuao"), "f12f52e45028e3a190ac6ec7653a8a6a9403abb0f36d313c824c3200e9825c846a334139c8db9f2cec6eec87c4644224c6eda9d895b18fe3bd7a7964869e0c31");
     }
 }
